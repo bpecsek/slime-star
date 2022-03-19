83fe566813d68ce1187c5d430f1538a3a149a14a
Mariano Montone <marianomontone@gmail.com>
2022-03-19 16:50:34 -0300

    slime-star contrib

---
+++ contrib/slime-star.el
@@ -0,0 +1,40 @@
+(require 'slime)
+
+(defun slime-room ()
+  (interactive)
+  (let ((room-str
+	 (slime-eval `(cl:with-output-to-string (cl:*standard-output*)
+						(cl:room)))))
+    (let ((buffer (get-buffer-create "*slime-room*")))
+      (with-current-buffer buffer
+	(insert room-str)
+	(display-buffer buffer)))))
+
+(defun slime-star-setup-key-bindings ()
+  )
+
+(defun slime-star--setup-menus ()
+  (easy-menu-add-item 'menubar-slime nil "Tools" "Documentation")
+  (easy-menu-add-item 'menubar-slime '("Tools")
+		      ["System Browser" lisp-system-browser])
+
+  (easy-menu-add-item 'menubar-slime '("Tools")
+		      ["Quicklisp Systems" quicklisp-systems])
+
+  (easy-menu-add-item 'menubar-slime '("Tools")
+		      ["Quicksearch" quicksearch]))
+
+(define-slime-contrib slime-star
+  "SLIME with extra extensions preinstalled."
+  (:authors "Mariano Montone")
+  (:license "GPL")
+  (:slime-dependencies quicklisp-systems slime-help system-browser)
+  (:swank-dependencies slime-star)
+  (:on-load
+   ;; setup key bindings
+   (slime-star-setup-key-bindings)
+   ;; add submenu to SLIME menu
+   ;;(slime-help--add-menu-to-slime)
+   (slime-star--setup-menus)))
+
+(provide 'slime-star)
