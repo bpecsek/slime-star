67c4a93c3b29839dc199e7a551bb656a58ad55c8
Mariano Montone <marianomontone@gmail.com>
2022-03-19 17:53:44 -0300

    slime-breakpoints

--- contrib/slime-star.el
+++ contrib/slime-star.el
@@ -28,7 +28,7 @@
   "SLIME with extra extensions preinstalled."
   (:authors "Mariano Montone")
   (:license "GPL")
-  (:slime-dependencies quicklisp-systems slime-help system-browser)
+  (:slime-dependencies quicklisp-systems slime-help system-browser slime-breakpoints)
   (:swank-dependencies slime-star)
   (:on-load
    ;; setup key bindings
